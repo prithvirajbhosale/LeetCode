c213172aaf305c38c06aafad13c19663e28d8182
prithviBhosale <bhosaleprithvi96@gmail.com>
2023-12-12 22:54:25 -0500

    Two sum

---
+++ src/main/java/Arrays/TwoSum.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+
+
+            HashMap<Integer,Integer> map = new HashMap<>();
+            for(int i=0; i<nums.length ; i++){
+                int num = nums[i];
+                int diff = target - num;
+                if(map.containsKey(diff)){
+                    return new int[] { map.get(diff), i };
+                }
+                map.put(num,i);
+            }
+            return new int[] {};
+        }
+    }
+}
