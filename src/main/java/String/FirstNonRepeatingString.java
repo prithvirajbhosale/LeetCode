26b382e2812ba5c4b5721c6f48cbe819ac9f8291
prithviBhosale <bhosaleprithvi96@gmail.com>
2024-02-10 10:15:18 -0500

    First non reepating character in a string

--- src/main/java/String/FirstNonRepeatingString.java
+++ src/main/java/String/FirstNonRepeatingString.java
@@ -12,38 +12,66 @@ Output: f
 Explanation: As ‘f’ is first character in the string which does not repeat.
  */
 public class FirstNonRepeatingString {
+    static final int NO_OF_CHARS = 256;
+    static char count[] = new char[NO_OF_CHARS];
 
     public static void main(String[] args) {
         firstNonRepeatingString("geeksforgeeks");
+        System.out.println("First repeating character is " + firstNonRepeatingStringwithHashmap("geeksforgeeks"));
+        System.out.println("First repeating character is via brute force approach is  " + firstNonRepeatingStringBruteForce("geeksforgeeks"));
     }
 
-    class CountIndex {
-        int count, index;
+    static int firstNonRepeatingString(String str) {
 
-        // constructor for first occurrence
-        public CountIndex(int index)
-        {
-            this.count = 1;
-            this.index = index;
-        }
+        getCharCountArray(str);
+        int index = -1, i;
 
-        // method for updating count
-        public void incCount() { this.count++; }
-    }
-
-     static int firstNonRepeatingString(String s){
-
-        int count =0;
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for(int i =0; i<s.length(); i++){
-            if(map.containsKey(s.charAt(i))){
-               map.get(s.charAt(i));
-            }else{
-                map.put(s.charAt(i),++count);
+        for (i = 0; i < str.length(); i++) {
+            if (count[str.charAt(i)] == 1) {
+                index = i;
+                break;
             }
-
         }
-        return count;
+
+        return index;
+    }
+
+    static void getCharCountArray(String str) {
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i)]++;
+    }
+
+    public static Character firstNonRepeatingStringwithHashmap(String str) {
+        //O(1)
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            mp.put(str.charAt(i), mp.getOrDefault(str.charAt(i), 0) + 1);
+        }
+        for (Character c : str.toCharArray()) {
+            if (mp.get(c) == 1) {
+                return c;
+            }
+        }
+        return null;
+
+    }
+
+    public static Character firstNonRepeatingStringBruteForce(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            boolean repeat = false;
+            for (int j = 0; j < str.length(); j++) {
+                if (i != j && str.charAt(i) == str.charAt(j)) {
+                    repeat = true;
+                    break;
+                }
+            }
+            if (!repeat) {
+                return str.charAt(i);
+            }
+        }
+        return null;
+
     }
 }
+ 
